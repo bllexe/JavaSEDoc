af29e2a0df13929f4ff1ecc41581ffc6eabc3cb7
bllexe <bilalykt21@gmail.com>
2024-07-09 14:15:22 +0300

    javadoc command

---
+++ src/main/java/org/tigris/javadoc/Main.java
@@ -0,0 +1,15 @@
+package org.tigris.javadoc;
+
+public class Main {
+
+        /**
+         * @param number1
+         * @param number2
+         * @return sum of two numbers
+         */
+        public int  sum(int number1, int number2){
+            return number2 + number1;
+
+    }
+    //open terminal with directory run javadoc Main.java for documentation
+}
